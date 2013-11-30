dfe2e9a645092240c939ed39cfa83add5b831401
Harold Schreckengost <tug@alleghenycyclery.com>
2013-11-29 22:44:53 -0500

    After Generation of BikeFit Rails App
    Yeah, man.  WOO.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BikeFit::Application.config.secret_key_base = '8f8083f034a2958a4d099543e3e83515b5c3fdd7dc09620c44e7ae3ba6408f29c48f40e325443cd6ea2e63f8a1b64c6b0d7a2088927231ea3b8f30c7857fa773'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BikeFit::Application.config.session_store :cookie_store, key: '_BikeFit_session'
