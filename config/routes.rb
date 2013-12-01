5ff04075d89ce0ec6867c69a8c7c14eca0d5d0e9
Harold Schreckengost <tug@alleghenycyclery.com>
2013-11-30 21:51:04 -0500

    11.30.13 950PM
    Sign-in, sign-out verified to be working fine!  SWEET!

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ BikeFit::Application.routes.draw do
   devise_for :users
   get "home/index"
 	root :to => "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -26,7 +27,7 @@ BikeFit::Application.routes.draw do
   #     end
   #
   #     collection do
-  #       get 'sold'
+  #       get 'so
   #     end
   #   end
 
