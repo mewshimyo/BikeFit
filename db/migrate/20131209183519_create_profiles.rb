2c29a4219422f0606854faf6c2c2b90032529c2e
mewshimyo <tug@alleghenycyclery.com>
2013-12-09 13:40:13 -0500

    Profile Model created
    Oh god o.o'

--- db/migrate/20131209183519_create_profiles.rb
+++ db/migrate/20131209183519_create_profiles.rb
@@ -1,13 +1,12 @@
 class CreateProfiles < ActiveRecord::Migration
   def change
     create_table :profiles do |t|
-      t.integer :id
+      t.integer :user_id
       t.string :first_name
       t.string :last_name
       t.date :birthday
 
       t.timestamps
     end
-    add_index :profiles, :id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130225303) do
+ActiveRecord::Schema.define(version: 20131209183519) do
+
+  create_table "profiles", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "birthday"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
