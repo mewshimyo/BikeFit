93b44d4d8fd23ef243fc9410b131e1e48db44455
Harold Schreckengost <tug@alleghenycyclery.com>
2013-12-12 00:44:19 -0500

    Some more stuff :P

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209183519) do
+ActiveRecord::Schema.define(version: 20131211172416) do
 
   create_table "profiles", force: true do |t|
     t.integer  "user_id"
